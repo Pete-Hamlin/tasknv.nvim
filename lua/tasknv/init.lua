1dcf46799769d9342d703ce692fa6e2e73bc0d04
pete <work@pete-hamlin.com>
2026-04-18 21:03:49 +0100

    feat: wire up autocmd and expose sync API

--- lua/tasknv/init.lua
+++ lua/tasknv/init.lua
@@ -7,6 +7,22 @@ function M.setup(opts)
 	vim.api.nvim_create_user_command("AnalyzeMarkdown", function()
 		require("tasknv.parser").parse()
 	end, { desc = "Analyze Markdown file for tasks and headings" })
+
+	if opts.sync_on_save ~= false then
+		local group = vim.api.nvim_create_augroup("tasknv", { clear = true })
+
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			group = group,
+			callback = function(args)
+				local debounce_ms = require("tasknv.config").sync_debounce_ms or 500
+				require("tasknv.sync").debounced_sync(args.buf, debounce_ms)
+			end,
+		})
+	end
+end
+
+function M.sync(opts)
+	return require("tasknv.sync").sync(opts)
 end
 
 return M
